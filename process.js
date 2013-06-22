70be626ab742c0bff354a0aff2e227c50dd22d40
Daniel Feusse <dfeusse@gmail.com>
2013-06-21 19:33:45 -0700

    start of node data structures

--- process.js
+++ process.js
@@ -10,6 +10,15 @@ function process(data) {
       nameId = "id_" + d._id;
     }
 
+    var nodeScore;
+    if (d.attended) 
+    {
+      nodeScore = 1
+        } else {
+          nodeScore = -1;
+        } 
+      
+
     node = {
       id: nameId,
       //radius: radius_scale(parseInt(d.total_amount, 10)),
@@ -20,9 +29,37 @@ function process(data) {
       x: Math.random() * 900,
       y: Math.random() * 800,
       //meetup: d.event_id,
+      attended: d.attended, //ADDED
+      node_score: nodeScore, //ADDED
+      //node_array: [],
       node_centers: d.event.id + "_" + d.attended
     };
+
+
     nodes.push(node);
   });
   nodes.sort(function(a,b) {return b.value - a.value; });
+
+var node_object = {};
+for (var i=0; i<nodes.length; i++) {
+      var s = nodes[i];
+      s = [s.id];
+        }
+    
+
+/*
+for (var i=0; i<nodes.length; i++) {
+  var s = nodes[i];
+  console.log(s.id);
+  node_object.id = s.id
+}
+*/
+
+} // end of process function
+
+
+function nodeArrays(data) {
+    data.forEach(function(d) {
+
+    })
 }
